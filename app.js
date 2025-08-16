0ec34f2f426ce8c80a6ac032f5eb95f50d0cb2b3
Felipe Maciel <felipemacielgm@gmail.com>
2025-08-16 20:17:21 -0300

    Adição de comentários e melhorias no código

--- app.js
+++ app.js
@@ -1,44 +1,15 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaNomes = [];
 let nomeInput = document.getElementById('amigo');
-let sorteioState = false;
-
-
-function exibirLista() {
-   let lista = document.getElementById('listaNomes');
-   lista.innerHTML = '';
-   listaNomes.forEach((nome, i) => {
-      let li = document.createElement('li');
-      li.textContent = nome;
-      li.classList.add('amigo-item');
-
-      li.addEventListener('click', () => {
-         removerAmigo(i);
-      });
-      lista.appendChild(li);
-
-   });
-}
-
-
-
-
-function removerAmigo(i) {
-   listaNomes.splice(i, 1);
-   exibirLista();
-}
-
-
-
-
+let sorteioState = false;    //variável para monitorar o estado do sorteio
 
 function adicionarAmigo (){
-   let nome = nomeInput.value.trim();
+   let nome = nomeInput.value.trim();  //.trim para remover espaços indesejáveis
    if (nome === ''){
       alert('Insira um nome!');
       return;
    }
-   
+
+   /* resetar o estado caso já houve o sorteio */   
    if (sorteioState){
       document.getElementById('resultado').innerHTML = '';
       sorteioState = false;
@@ -48,6 +19,31 @@ function adicionarAmigo (){
    exibirLista();
 
 }
+
+function exibirLista() {
+   let lista = document.getElementById('listaAmigos');
+   lista.innerHTML = '';
+   listaNomes.forEach((nome, i) => {
+      let li = document.createElement('li');
+      li.textContent = nome;
+      li.classList.add('amigo-item');
+
+      /* funcionalidade extra que permite retirar o nome de alguém da lista caso seja adicionado por engano */
+      li.addEventListener('click', () => {
+         removerAmigo(i);
+      });
+      lista.appendChild(li);
+
+   });
+}
+
+/* função extra para remover um nome da lista */
+function removerAmigo(i) {
+   listaNomes.splice(i, 1);
+   exibirLista();
+}
+
+
 
 function sortearAmigo() {
    if(listaNomes.length === 0) {
